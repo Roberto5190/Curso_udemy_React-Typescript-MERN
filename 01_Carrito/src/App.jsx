19f1b01345f49e4061af3d8a31fe643ec6687ba0
Roberto5190 <rfernandez5190@esdmadrid.es>
2025-01-21 08:53:23 +0100

    custom file project, add sass

--- 01_Carrito/src/App.jsx
+++ 01_Carrito/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/sass/app.sass";
+import logoIrasi from "/logo_irasi.png"
+import cartIcon from "/cart_icon.svg"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+              <header className="header">
+        <div className="header_left">
+          <a className="logo" href="index.html">
+            <img className="logo_irasi" src={logoIrasi} alt="" />
+          </a>
+
+          <nav className="header_nav">
+            <a className="header_shop" href="#">Tienda</a>
+            <a className="header_about" href="#">Sobre nosotros</a>
+            <a className="header_contact" href="#">Contacto</a>
+          </nav>
+        </div>
+
+        <div className="header_right">
+          <div className="cart_button">
+          <img className="cart_icon" src={cartIcon} alt="" />
+          </div>
+        </div>
+      </header>
+
+      
+      <section className="hero_section">
+
+      </section>
     </>
   )
 }
